ed0e7fc85a7865e9056cca210911b8fb4ff7802a
olivia1117 <oadamic@gmail.com>
2025-11-09 19:34:50 -0600

    plugging in the correct server URL and adding functionality so that it will automatically generate a die roll when the user reloads the page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Function to roll the die and update the image
 async function rollDice() {
     try {
-        const response = await fetch('https://<your-server-url>/api/rollDice');
+        const response = await fetch('https://dice-roller-2-server-frf4d9axesg7hucc.centralus-01.azurewebsites.net/api/rollDice');
         const data = await response.json();
 
         // Update the image
