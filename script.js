b85a4bf88e7bc175f100bfbd0422e3148813072f
olivia1117 <oadamic@gmail.com>
2025-11-09 20:20:31 -0600

    editing script.js to include comments

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Function to roll the die and update the image
+// function to roll the die and update the image
 async function rollDice() {
     try {
         const response = await fetch('https://dice-roller-2-server-frf4d9axesg7hucc.centralus-01.azurewebsites.net/api/rollDice');
         const data = await response.json();
 
-        // Update the image
+        // update the image
         document.getElementById('die').src = `/img/${data.image}`;
         console.log(`Rolled a ${data.roll}`);
     } catch (error) {
@@ -12,6 +12,6 @@ async function rollDice() {
     }
 }
 
-// Focus the roll button when the page loads
+// focus the roll button when the page loads
 document.getElementById("rollButton").focus();
 
